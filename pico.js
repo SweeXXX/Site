87c9a803c60eb974293cd8f048d107e06c7d2b09
Nikita Poletaev <nikitapoletaev44941@mail.ru>
2023-08-12 19:58:15 +0300

    create site

---
+++ pico.js
@@ -0,0 +1,20 @@
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+
+      // поменять элементы местами
+      // мы используем для этого синтаксис "деструктурирующее присваивание"
+      // подробнее о нём - в следующих главах
+      // то же самое можно записать как:
+      // let t = array[i]; array[i] = array[j]; array[j] = t
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+function testDateTime(a, b) {
+    // Тут нужно написать решение
+      let n = new Date(a),
+          m = new Date(b),
+          arr = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+      console.log(arr[getDay(n-m)]);
+  }
+  
